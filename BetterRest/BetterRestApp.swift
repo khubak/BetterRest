4eee32a19733948d458ff96109d116965c553d1e
khubak <103487011+khubak@users.noreply.github.com>
2022-11-28 14:24:52 +0100

    BetterRest

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Karlo Hubak on 28.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
